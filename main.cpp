67b893e75dfc32a69eee423aa397cca6be238b52
BK-201 <pityunin.aleksander@yandex.ru>
2022-06-22 20:24:30 +0300

    added new line to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 int main ()
 {
 	std::cout << "Hello world!" << std::endl;
+	std::cout << "Testing" << std::endl;
+	std::cout << "Don't we need a special sign to end line on screen?" << std::endl;
 	return 0;
 }
